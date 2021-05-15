5ac684e8473db4efcd5c4fb283c798441866699b
nickguerra <nickg@maliciousgames.com>
2021-05-15 07:38:23 -0500

    first commit

---
+++ BlockChain/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BlockChain
+{
+    class Program
+    {
+        public interface IBlock
+        {
+            byte[] Data { get; }
+            byte[] Hash { get; set; }
+            int Nonce { get; set; }
+            byte[] PrevHash { get; set; }
+            DateTime TimeStamp { get; }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
