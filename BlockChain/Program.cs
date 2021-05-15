851a71535e17feaa2b844704484d88d2d0320604
nickguerra <nickg@maliciousgames.com>
2021-05-15 07:46:30 -0500

    added bitconverter method

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -13,6 +13,21 @@ namespace BlockChain
             DateTime TimeStamp { get; }
         }
 
+        public class Block : IBlock
+        {
+            public byte[] Data { get; }
+
+            public byte[] Hash { get; set; }
+            public int Nonce { get; set; }
+            public byte[] PrevHash { get; set; }
+            public DateTime TimeStamp { get; }
+
+            public override string ToString()
+            {
+                return $"{BitConverter.ToString(Hash).Replace("-", "")}:\n{BitConverter.ToString(PrevHash).Replace("-", "")}";
+            }
+        }
+
 
 
         static void Main(string[] args)
