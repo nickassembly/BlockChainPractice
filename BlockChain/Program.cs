82bb88d9d129d1ca64c599abce18a7a0ec721972
nickguerra <nickg@maliciousgames.com>
2021-05-18 14:25:48 -0500

    finished implementing functions for block chain simulation

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -24,10 +24,40 @@ namespace BlockChain
                 return sha.ComputeHash(starr);
             }
         }
-        public static byte[] MineHash(this IBlock block, byte[] difficulty) { }
-        public static bool IsValid(this IBlock block) { }
-        public static bool IsValidPrevBlock(this IBlock block, IBlock prevBlock) { }
-        public static bool IsValid(this IEnumerable<IBlock> items) { }
+        public static byte[] MineHash(this IBlock block, byte[] difficulty) 
+        {
+            if (difficulty == null) throw new ArgumentNullException(nameof(difficulty));
+
+            byte[] hash = new byte[0];
+            int d = difficulty.Length;
+            while (!hash.Take(2).SequenceEqual(difficulty))
+            {
+                block.Nonce++;
+                hash = block.GenerateHash();
+            }
+
+            return hash;
+        }
+
+        public static bool IsValid(this IBlock block) 
+        {
+            var bk = block.GenerateHash();
+            return block.Hash.SequenceEqual(bk);
+        }
+
+        public static bool IsValidPrevBlock(this IBlock block, IBlock prevBlock) 
+        {
+            if (prevBlock == null) throw new ArgumentNullException(nameof(prevBlock));
+
+            var prev = prevBlock.GenerateHash();
+            return prevBlock.IsValid() && block.PrevHash.SequenceEqual(prev);
+        }
+
+        public static bool IsValid(this IEnumerable<IBlock> items)
+        {
+            var enumerable = items.ToList();
+            return enumerable.Zip(enumerable.Skip(1), Tuple.Create).All(block => block.Item2.IsValid() && block.Item2.IsValidPrevBlock(block.Item1));
+        }
     }
 
     public interface IBlock
@@ -39,75 +69,98 @@ namespace BlockChain
         DateTime TimeStamp { get; }
     }
 
+    public class Block : IBlock
+    {
+        public Block(byte[] data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+            Nonce = 0;
+            PrevHash = new byte[] { 0x00 };
+            TimeStamp = DateTime.Now;
+        }
+
+        public byte[] Data { get; }
+        public byte[] Hash { get; set; }
+        public int Nonce { get; set; }
+        public byte[] PrevHash { get; set; }
+        public DateTime TimeStamp { get; }
+
+        public override string ToString()
+        {
+            return $"{BitConverter.ToString(Hash).Replace("-", "")}:\n{BitConverter.ToString(PrevHash).Replace("-", "")}\n {Nonce} {TimeStamp}";
+        }
+    }
+
+    public class BlockChain : IEnumerable<IBlock>
+    {
+        private List<IBlock> _items = new List<IBlock>();
+
+        public BlockChain(byte[] difficulty, IBlock genesis)
+        {
+            Difficulty = difficulty;
+            genesis.Hash = genesis.MineHash(difficulty);
+            Items.Add(genesis);
+        }
+
+        public void Add(IBlock item)
+        {
+            if (Items.LastOrDefault() != null)
+            {
+                item.PrevHash = Items.LastOrDefault()?.Hash;
+            }
+            item.Hash = item.MineHash(Difficulty);
+            Items.Add(item);
+        }
+
+        public int Count => Items.Count;
+        public IBlock this[int index]
+        {
+            get => Items[index];
+            set => Items[index] = value;
+        }
+
+        public byte[] Difficulty { get; }
+
+        public List<IBlock> Items
+        {
+            get => _items;
+            set => _items = value;
+        }
+
+        public IEnumerator<IBlock> GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return Items.GetEnumerator();
+        }
+    }
+
     class Program
     {
-       
-
-        public class Block : IBlock
-        {
-            public byte[] Data { get; }
-
-            public byte[] Hash { get; set; }
-            public int Nonce { get; set; }
-            public byte[] PrevHash { get; set; }
-            public DateTime TimeStamp { get; }
-
-            public override string ToString()
-            {
-                return $"{BitConverter.ToString(Hash).Replace("-", "")}:\n{BitConverter.ToString(PrevHash).Replace("-", "")}\n {Nonce} {TimeStamp}";
-            }
-        }
-
-        public class BlockChain : IEnumerable<IBlock>
-        {
-            private List<IBlock> _items = new List<IBlock>();
-
-            public BlockChain(byte[] difficulty, IBlock genesis)
-            {
-                Difficulty = difficulty;
-                genesis.Hash = genesis.MineHash(difficulty);
-                Items.Add(genesis);
-            }
-
-            public void Add(IBlock item)
-            {
-                if (Items.LastOrDefault() != null)
-                {
-                    item.PrevHash = Items.LastOrDefault()?.Hash;
-                }
-                item.Hash = item.MineHash(Difficulty);
-                Items.Add(item);
-            }
-
-            public int Count => Items.Count;
-            public IBlock this[int index]
-            {
-                get => Items[index];
-                set => Items[index] = value;
-            }
-
-            public byte[] Difficulty { get;  }
-
-            public List<IBlock> Items
-            {
-                get => _items;
-                set => _items = value;
-            }
-
-            public IEnumerator<IBlock> GetEnumerator()
-            {
-                return Items.GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return Items.GetEnumerator();
-            }
-        }
 
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random(DateTime.UtcNow.Millisecond);
+            IBlock genesis = new Block(new byte[] {0x00, 0x00, 0x00, 0x00, 0x00 });
+            byte[] difficulty = new byte[] { 0x00, 0x00 };
+
+            BlockChain chain = new BlockChain(difficulty, genesis);
+            for (int i = 0; i < 200; i++)
+            {
+                var data = Enumerable.Range(0, 2256).Select(p => (byte)rnd.Next());
+                chain.Add(new Block(data.ToArray()));
+                Console.WriteLine(chain.LastOrDefault()?.ToString());
+
+                Console.WriteLine($"Chain is valid: {chain.IsValid()}");
+            }
+
+            Console.ReadLine();
+
+
         }
+
     }
 }
