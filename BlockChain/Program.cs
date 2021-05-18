c6f937b42ab937372d60a697d69ce43bfadae27d
nickguerra <nickg@maliciousgames.com>
2021-05-17 19:40:49 -0500

    Finished extension method

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using static BlockChain.Program;
 
 namespace BlockChain
 {
+    public static class BlockChainExtension
+    {
+        public static byte[] GenerateHash(this IBlock block)
+        {
+            using (SHA512 sha = new SHA512Managed())
+            using (MemoryStream st = new MemoryStream())
+            using (BinaryWriter bw = new BinaryWriter(st))
+            {
+                bw.Write(block.Data);
+                bw.Write(block.Nonce);
+                bw.Write(block.TimeStamp.ToBinary());
+                bw.Write(block.PrevHash);
+                var starr = st.ToArray();
+                return sha.ComputeHash(starr);
+            }
+        }
+        public static byte[] MineHash(this IBlock block, byte[] difficulty) { }
+        public static bool IsValid(this IBlock block) { }
+        public static bool IsValidPrevBlock(this IBlock block, IBlock prevBlock) { }
+        public static bool IsValid(this IEnumerable<IBlock> items) { }
+    }
+
+    public interface IBlock
+    {
+        byte[] Data { get; }
+        byte[] Hash { get; set; }
+        int Nonce { get; set; }
+        byte[] PrevHash { get; set; }
+        DateTime TimeStamp { get; }
+    }
+
     class Program
     {
-        public interface IBlock
-        {
-            byte[] Data { get; }
-            byte[] Hash { get; set; }
-            int Nonce { get; set; }
-            byte[] PrevHash { get; set; }
-            DateTime TimeStamp { get; }
-        }
+       
 
         public class Block : IBlock
         {
@@ -76,15 +103,6 @@ namespace BlockChain
             {
                 return Items.GetEnumerator();
             }
-        }
-
-        public static class BlockChainExtension
-        {
-            public static byte[] GenerateHash(this IBlock block) { }
-            public static byte[] MineHash(this IBlock block, byte[] difficulty) { } 
-            public static bool IsValid(this IBlock block) { }
-            public static bool IsValidPrevBlock (this IBlock block, IBlock prevBlock) { }
-            public static bool IsValid(this IEnumerable<IBlock> items) { }
         }
 
         static void Main(string[] args)
